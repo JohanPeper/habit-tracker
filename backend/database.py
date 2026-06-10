1fff24b96489617b8c1bd67f11a2c5b03b0e0397
Ivan <70844716+JohanPeper@users.noreply.github.com>
2026-06-10 20:17:29 +0300

    Update database.py
    fix port in link

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("postgresql://postgres.cyetmgrbakuujdisphla:[UuSF8JKymzDkoWTy]@aws-0-eu-west-3.pooler.supabase.com:5432/postgres?sslmode=require")
+DATABASE_URL = os.getenv("postgresql://postgres.cyetmgrbakuujdisphla:[UuSF8JKymzDkoWTy]@aws-0-eu-west-3.pooler.supabase.com:6543/postgres?sslmode=require")
 # Если DATABASE_URL не задан, используем локальный SQLite для разработки
 if not DATABASE_URL:
     DATABASE_URL = "sqlite:///./habits.db"
